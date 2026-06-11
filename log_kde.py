aa451bce4a4f0692f0156428d1b387ddbb1ef1f3
Calvin Nguyen <calvinnguyen@Mac-31257.lan>
2026-06-11 16:10:18 -0700

    Added log_kde.py for log_kde object and functions for simplicity.

---
+++ log_kde.py
@@ -0,0 +1,54 @@
+import numpy as np
+import quantecon as qe
+from numpy.typing import NDArray
+from scipy.linalg import expm
+from scipy.special import kl_div, logsumexp
+from tqdm import tqdm
+class log_kde():
+    def __init__(self, P, t):
+        self.I = np.eye(P.shape[0])
+        self.P = P
+        self.Pt = expm(-t * (self.I - P))
+        self.pi = qe.MarkovChain(P).stationary_distributions
+        self.logPt = np.log(self.Pt)
+
+    def compute_log_kde(self, p) -> NDArray:
+        log_kde_sum = np.log(sum([np.exp(-kl_div(p, row).sum()) for row in self.Pt])) # need to sum kl_div, returns array
+        return log_kde_sum
+
+    def calculate_ti(self, p_i):
+        # construct t_i: using KL divergence in a different way
+        scores = self.logPt @ p_i + np.log(self.pi)        # (n,)
+        t = np.exp(scores - logsumexp(scores))
+        return t
+
+    def calculate_r(self, p_i):
+        t = self.calculate_ti(p_i)
+        r = np.zeros(shape=self.Pt[0].shape)
+        # construct r
+        for t_i, log_p_i in zip(t, self.logPt):
+            r_i = t_i * log_p_i             # scalar * log(array) = array
+            r += r_i
+        return r
+
+    def calculate_q(self, p_i):
+        r = self.calculate_r(p_i)
+        q_tilde = np.exp(r)
+        q = -np.log(q_tilde / q_tilde.sum())
+        return q
+    
+    def calculate_R_mat(self):
+        R = []
+        for p_i in self.Pt:
+            r = self.calculate_r(p_i)
+            R.append(r)
+        return np.vstack(R)
+    
+    def calculate_Q_mat(self):
+        Q = []
+        for p_i in self.Pt:
+            q = self.calculate_q(p_i)
+            Q.append(q)
+        return np.vstack(Q)
+
+    
